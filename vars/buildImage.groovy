e9e7e95754176133025b3b8bd33f3fef11fa7258
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-08-20 16:20:51 +0530

    added Docker Class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,26 +1,7 @@
 #!/usr/bin/env groovy
 
+import com.docker.Docker
+
 def call(String imageName) {
-    echo "Building docker image"
-
-    withCredentials([usernamePassword(
-            // The ID used to store the credentials in Jenkins
-            credentialsId: 'docker-hub-credentials',
-            // The variable that will hold the password from the credentials
-            passwordVariable: 'DOCKER_HUB_PASSWORD',
-            // The variable that will hold the username from the credentials
-            usernameVariable: 'DOCKER_HUB_USERNAME')]) {
-        // Execute a shell command to build a Docker image
-        sh "docker build -t $imageName ."
-
-        // Execute a shell command to log in to Docker Hub
-        // The `echo $DOCKER_HUB_PASSWORD` part is used to pass the password to the `docker login` command
-        // The `-u` option is used to specify the username
-        // The `--password-stdin` option tells Docker to read the password from the standard input
-        sh "echo $DOCKER_HUB_PASSWORD | docker login -u $DOCKER_HUB_USERNAME --password-stdin"
-
-        // Execute a shell command to push the Docker image to Docker Hub
-        sh "docker push $imageName"
-    }
-
+    return new Docker(this).buildDockerImage(imageName)
 }
